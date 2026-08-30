85826713b9b4728cd6b7809de6e97c71173fcab3
my9761313328-ops <my9761313328@gmail.com>
2026-08-30 17:21:07 +0530

    Bulk upload: app/src/main/java/com/nihalthakral/nihalhome/OfflineGameActivity.kt

---
+++ app/src/main/java/com/nihalthakral/nihalhome/OfflineGameActivity.kt
@@ -0,0 +1,12 @@
+package com.nihalthakral.nihalhome
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class OfflineGameActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_offline_game)
+    }
+}
