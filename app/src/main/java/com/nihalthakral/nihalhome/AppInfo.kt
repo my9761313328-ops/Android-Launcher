4bbec0f9e62aea90e6234dcac04c38cc9a3858b8
my9761313328-ops <my9761313328@gmail.com>
2026-08-31 18:49:32 +0530

    Bulk upload: app/src/main/java/com/nihalthakral/nihalhome/AppInfo.kt

---
+++ app/src/main/java/com/nihalthakral/nihalhome/AppInfo.kt
@@ -0,0 +1,10 @@
+package com.nihalthakral.nihalhome
+
+import android.graphics.drawable.Drawable
+
+data class AppInfo(
+    val label: String,
+    val packageName: String,
+    val activityName: String,
+    val icon: Drawable
+)
