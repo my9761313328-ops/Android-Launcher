ef534e06a825356a641e7456cad67f8ddaf09d2e
my9761313328-ops <my9761313328@gmail.com>
2026-09-01 13:21:03 +0530

    Bulk upload: app/src/main/java/com/nihalthakral/nihalhome/OfflineGameActivity.kt

--- app/src/main/java/com/nihalthakral/nihalhome/OfflineGameActivity.kt
+++ app/src/main/java/com/nihalthakral/nihalhome/OfflineGameActivity.kt
@@ -1,12 +1,33 @@
 package com.nihalthakral.nihalhome
 
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.activity.ComponentActivity
 
 class OfflineGameActivity : ComponentActivity() {
 
+    private var gameWebView: WebView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_offline_game)
+
+        val webView = findViewById<WebView>(R.id.offlineGameWebView)
+        gameWebView = webView
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.loadUrl("file:///android_asset/game/offline_game.html")
+    }
+
+    override fun onDestroy() {
+        gameWebView?.apply {
+            stopLoading()
+            loadUrl("about:blank")
+            removeAllViews()
+            destroy()
+        }
+        gameWebView = null
+        super.onDestroy()
     }
 }
