24fd95a165e610bb9db32a0116f03365c1438b7e
my9761313328-ops <my9761313328@gmail.com>
2026-09-06 14:51:31 +0530

    Bulk upload: app/src/main/java/com/nihalthakral/nihalhome/MainActivity.kt

--- app/src/main/java/com/nihalthakral/nihalhome/MainActivity.kt
+++ app/src/main/java/com/nihalthakral/nihalhome/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.nihalthakral.nihalhome
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val prefs = getSharedPreferences(PreferenceKeys.PREFS_NAME, MODE_PRIVATE)
+        val languageSelected = prefs.contains(PreferenceKeys.KEY_SELECTED_LANGUAGE)
+
+        if (!languageSelected) {
+            startActivity(Intent(this, LanguageSelectionActivity::class.java))
+            finish()
+            return
+        }
+
+        if (!LauncherUtils.isDefaultLauncher(this)) {
+            startActivity(Intent(this, DefaultLauncherActivity::class.java))
+            finish()
+            return
+        }
+
         setContentView(R.layout.activity_main)
     }
 }
