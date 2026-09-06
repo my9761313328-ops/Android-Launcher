6233269343c1e9135cdd533d2c6bd62c1459500d
my9761313328-ops <my9761313328@gmail.com>
2026-09-06 14:51:25 +0530

    Bulk upload: app/src/main/java/com/nihalthakral/nihalhome/PreferenceKeys.kt

---
+++ app/src/main/java/com/nihalthakral/nihalhome/PreferenceKeys.kt
@@ -0,0 +1,8 @@
+package com.nihalthakral.nihalhome
+
+object PreferenceKeys {
+    const val PREFS_NAME = "nihal_home_prefs"
+    const val KEY_SELECTED_LANGUAGE = "selected_language"
+    const val LANGUAGE_ENGLISH = "en"
+    const val LANGUAGE_HINDI_URDU = "hi_ur"
+}
