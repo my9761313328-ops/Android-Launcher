11b471d09350572e74eeb552ad9899e76ae8ded5
my9761313328-ops <my9761313328@gmail.com>
2026-09-08 10:14:32 +0530

    Bulk upload: app/src/main/java/com/nihalthakral/nihalhome/UninstallAppsActivity.kt

---
+++ app/src/main/java/com/nihalthakral/nihalhome/UninstallAppsActivity.kt
@@ -0,0 +1,12 @@
+package com.nihalthakral.nihalhome
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class UninstallAppsActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_uninstall_apps)
+    }
+}
