2c4f8a9ef70c6f4e68ef63c9efdb0a918741c15d
my9761313328-ops <my9761313328@gmail.com>
2026-09-08 10:14:28 +0530

    Bulk upload: app/src/main/java/com/nihalthakral/nihalhome/OtpSmsActivity.kt

---
+++ app/src/main/java/com/nihalthakral/nihalhome/OtpSmsActivity.kt
@@ -0,0 +1,12 @@
+package com.nihalthakral.nihalhome
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class OtpSmsActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp_sms)
+    }
+}
