2187600f382cd2a786409d5f51abb1da175ce666
my9761313328-ops <my9761313328@gmail.com>
2026-09-08 22:08:05 +0530

    Bulk upload: app/src/main/java/com/nihalthakral/nihalhome/AskNihalAiActivity.kt

---
+++ app/src/main/java/com/nihalthakral/nihalhome/AskNihalAiActivity.kt
@@ -0,0 +1,12 @@
+package com.nihalthakral.nihalhome
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class AskNihalAiActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ask_nihal_ai)
+    }
+}
