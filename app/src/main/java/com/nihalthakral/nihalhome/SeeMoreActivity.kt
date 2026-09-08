852b20b9cd38533f9bd5592f0b46e295240cadbd
my9761313328-ops <my9761313328@gmail.com>
2026-09-08 22:42:20 +0530

    Bulk upload: app/src/main/java/com/nihalthakral/nihalhome/SeeMoreActivity.kt

--- app/src/main/java/com/nihalthakral/nihalhome/SeeMoreActivity.kt
+++ app/src/main/java/com/nihalthakral/nihalhome/SeeMoreActivity.kt
@@ -1,12 +1,27 @@
 package com.nihalthakral.nihalhome
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
 
 class SeeMoreActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_see_more)
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                goToMainScreen()
+            }
+        })
+    }
+
+    private fun goToMainScreen() {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        startActivity(intent)
+        finish()
     }
 }
