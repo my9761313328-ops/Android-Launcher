73b9f8863015160e1f8e926d8a2d8fda8fd0dce2
my9761313328-ops <my9761313328@gmail.com>
2026-09-08 10:14:36 +0530

    Bulk upload: app/src/main/java/com/nihalthakral/nihalhome/SeeMoreActivity.kt

---
+++ app/src/main/java/com/nihalthakral/nihalhome/SeeMoreActivity.kt
@@ -0,0 +1,12 @@
+package com.nihalthakral.nihalhome
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class SeeMoreActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_see_more)
+    }
+}
