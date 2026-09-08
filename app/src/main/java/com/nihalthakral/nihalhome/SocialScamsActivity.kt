5234dc5ac7e60b6229adb848cb1deffe6a2f53bd
my9761313328-ops <my9761313328@gmail.com>
2026-09-08 10:14:30 +0530

    Bulk upload: app/src/main/java/com/nihalthakral/nihalhome/SocialScamsActivity.kt

---
+++ app/src/main/java/com/nihalthakral/nihalhome/SocialScamsActivity.kt
@@ -0,0 +1,12 @@
+package com.nihalthakral.nihalhome
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class SocialScamsActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_social_scams)
+    }
+}
