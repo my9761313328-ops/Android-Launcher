d2bfd1666ab2e632f715aa5694946e0f224148e7
my9761313328-ops <my9761313328@gmail.com>
2026-09-14 07:09:22 +0530

    Bulk upload: app/src/main/java/com/nihalthakral/nihalhome/SocialHackingActivity.kt

--- app/src/main/java/com/nihalthakral/nihalhome/SocialHackingActivity.kt
+++ app/src/main/java/com/nihalthakral/nihalhome/SocialHackingActivity.kt
@@ -1,6 +1,7 @@
 package com.nihalthakral.nihalhome
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.webkit.WebSettings
@@ -8,6 +9,8 @@ import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedCallback
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class SocialHackingActivity : ComponentActivity() {
 
@@ -15,12 +18,22 @@ class SocialHackingActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
+
         setContentView(R.layout.activity_social_hacking)
 
+        hideSystemBars()
+
         webView = findViewById(R.id.webViewSocialHacking)
-        webView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        webView.setBackgroundColor(0xFF242C3F.toInt())
+        webView.overScrollMode = View.OVER_SCROLL_NEVER
+        webView.scrollBarStyle = View.SCROLLBARS_INSIDE_OVERLAY
+        webView.isScrollbarFadingEnabled = false
+        webView.isHapticFeedbackEnabled = false
+        webView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
 
         val settings: WebSettings = webView.settings
         settings.javaScriptEnabled = true
@@ -28,6 +41,9 @@ class SocialHackingActivity : ComponentActivity() {
         settings.loadWithOverviewMode = true
         settings.useWideViewPort = true
         settings.allowFileAccess = true
+        settings.builtInZoomControls = false
+        settings.displayZoomControls = false
+        settings.mediaPlaybackRequiresUserGesture = false
 
         webView.loadUrl("file:///android_asset/social_hacking/viewer.html")
 
@@ -36,6 +52,18 @@ class SocialHackingActivity : ComponentActivity() {
                 goToMainScreen()
             }
         })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemBars()
+    }
+
+    private fun hideSystemBars() {
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.systemBars())
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 
     private fun goToMainScreen() {
