48c0f1aa49ffcb52f8656633a8df1340f0e5f391
my9761313328-ops <my9761313328@gmail.com>
2026-09-14 06:45:46 +0530

    Bulk upload: app/src/main/java/com/nihalthakral/nihalhome/SocialHackingActivity.kt

--- app/src/main/java/com/nihalthakral/nihalhome/SocialHackingActivity.kt
+++ app/src/main/java/com/nihalthakral/nihalhome/SocialHackingActivity.kt
@@ -2,14 +2,34 @@ package com.nihalthakral.nihalhome
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebSettings
+import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedCallback
+import androidx.core.view.WindowCompat
 
 class SocialHackingActivity : ComponentActivity() {
 
+    private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_social_hacking)
+
+        webView = findViewById(R.id.webViewSocialHacking)
+        webView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+        val settings: WebSettings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.loadWithOverviewMode = true
+        settings.useWideViewPort = true
+        settings.allowFileAccess = true
+
+        webView.loadUrl("file:///android_asset/social_hacking/viewer.html")
 
         onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
