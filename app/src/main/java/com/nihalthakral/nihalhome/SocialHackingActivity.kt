e2bab19c8aa145cc16211186c6a5483152ea1d05
my9761313328-ops <my9761313328@gmail.com>
2026-09-14 07:34:05 +0530

    Bulk upload: app/src/main/java/com/nihalthakral/nihalhome/SocialHackingActivity.kt

--- app/src/main/java/com/nihalthakral/nihalhome/SocialHackingActivity.kt
+++ app/src/main/java/com/nihalthakral/nihalhome/SocialHackingActivity.kt
@@ -1,69 +1,21 @@
 package com.nihalthakral.nihalhome
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import android.view.View
-import android.webkit.WebSettings
-import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedCallback
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
 
 class SocialHackingActivity : ComponentActivity() {
 
-    private lateinit var webView: WebView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        window.statusBarColor = Color.TRANSPARENT
-        window.navigationBarColor = Color.TRANSPARENT
-
         setContentView(R.layout.activity_social_hacking)
-
-        hideSystemBars()
-
-        webView = findViewById(R.id.webViewSocialHacking)
-        webView.setBackgroundColor(0xFF242C3F.toInt())
-        webView.overScrollMode = View.OVER_SCROLL_NEVER
-        webView.scrollBarStyle = View.SCROLLBARS_INSIDE_OVERLAY
-        webView.isScrollbarFadingEnabled = false
-        webView.isHapticFeedbackEnabled = false
-        webView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
-
-        val settings: WebSettings = webView.settings
-        settings.javaScriptEnabled = true
-        settings.domStorageEnabled = true
-        settings.loadWithOverviewMode = true
-        settings.useWideViewPort = true
-        settings.allowFileAccess = true
-        settings.builtInZoomControls = false
-        settings.displayZoomControls = false
-        settings.mediaPlaybackRequiresUserGesture = false
-
-        webView.loadUrl("file:///android_asset/social_hacking/viewer.html")
 
         onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 goToMainScreen()
             }
         })
-    }
-
-    override fun onResume() {
-        super.onResume()
-        hideSystemBars()
-    }
-
-    private fun hideSystemBars() {
-        val controller = WindowInsetsControllerCompat(window, window.decorView)
-        controller.hide(WindowInsetsCompat.Type.systemBars())
-        controller.systemBarsBehavior =
-            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 
     private fun goToMainScreen() {
