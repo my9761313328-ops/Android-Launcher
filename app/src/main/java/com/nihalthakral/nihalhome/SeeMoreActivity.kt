9adf8ecbf3ff320bb22bfc3cd99c644ddccbc6b3
my9761313328-ops <my9761313328@gmail.com>
2026-09-17 11:34:27 +0530

    Bulk upload: app/src/main/java/com/nihalthakral/nihalhome/SeeMoreActivity.kt

--- app/src/main/java/com/nihalthakral/nihalhome/SeeMoreActivity.kt
+++ app/src/main/java/com/nihalthakral/nihalhome/SeeMoreActivity.kt
@@ -2,10 +2,6 @@ package com.nihalthakral.nihalhome
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.TypedValue
-import android.view.ViewTreeObserver
-import android.widget.FrameLayout
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedCallback
 
@@ -20,43 +16,6 @@ class SeeMoreActivity : ComponentActivity() {
                 goToMainScreen()
             }
         })
-
-        fitComingSoonText()
-    }
-
-    private fun fitComingSoonText() {
-        val root = findViewById<FrameLayout>(R.id.containerSeeMoreRoot)
-        val textView = findViewById<TextView>(R.id.textSeeMoreComingSoon)
-
-        root.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
-            override fun onGlobalLayout() {
-                val rootWidth = root.width
-                val rootHeight = root.height
-                if (rootWidth <= 0 || rootHeight <= 0) return
-
-                root.viewTreeObserver.removeOnGlobalLayoutListener(this)
-
-                val availableWidth = rootWidth * HORIZONTAL_CONTENT_FRACTION
-                val availableHeight = rootHeight * VERTICAL_CONTENT_FRACTION
-
-                val paint = textView.paint
-                val text = textView.text.toString()
-
-                var textSizePx = availableHeight
-                paint.textSize = textSizePx
-
-                while (textSizePx > 1f) {
-                    paint.textSize = textSizePx
-                    val fm = paint.fontMetrics
-                    val lineHeight = fm.bottom - fm.top
-                    val textWidth = paint.measureText(text)
-                    if (textWidth <= availableWidth && lineHeight <= availableHeight) break
-                    textSizePx -= 1f
-                }
-
-                textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSizePx)
-            }
-        })
     }
 
     private fun goToMainScreen() {
@@ -64,10 +23,5 @@ class SeeMoreActivity : ComponentActivity() {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
         startActivity(intent)
         finish()
-    }
-
-    companion object {
-        private const val HORIZONTAL_CONTENT_FRACTION = 0.8f
-        private const val VERTICAL_CONTENT_FRACTION = 0.8f
     }
 }
