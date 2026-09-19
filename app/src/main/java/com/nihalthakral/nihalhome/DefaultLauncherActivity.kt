38e9d96db21e7fa4a0809daaa3d7523a4e97e984
my9761313328-ops <my9761313328@gmail.com>
2026-09-19 21:09:46 +0530

    Bulk upload: app/src/main/java/com/nihalthakral/nihalhome/DefaultLauncherActivity.kt

--- app/src/main/java/com/nihalthakral/nihalhome/DefaultLauncherActivity.kt
+++ app/src/main/java/com/nihalthakral/nihalhome/DefaultLauncherActivity.kt
@@ -140,7 +140,6 @@ class DefaultLauncherActivity : ComponentActivity() {
 
     private fun proceedIfDefault() {
         if (LauncherUtils.isDefaultLauncher(this)) {
-            LauncherUtils.hideAppIcon(this)
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
